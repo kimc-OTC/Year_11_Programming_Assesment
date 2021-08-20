a536df3e036b2a94c8a50fd3bd6749351ee620f0
kimc-OTC <kimc@stu.otc.school.nz>
2021-08-21 00:34:58 +1200

    Wrote a code that provides currency list to the user and asks the type of currency from, to and amount to convert. All these processes of asking loops continuously until the user enters a valid input. However, because there are no still_converting = False, the code is an infinite loop for now.

--- Currency_converter.py
+++ Currency_converter.py
@@ -1,1 +1,36 @@
 # Currency Converter Program
+
+# list of currencies that are available for conversion
+currency_list = ["USD", "EUR", "JPY", "GBP", "AUD", "CAD", "CHF", "CNY", "SEK", "NZD", "MEX", "SGD", "HKD", "NOK", "KRW", "TRY", "INR", "RUB", "BRL", "ZAR"]
+# list of exchange rates: 1NZD to list of currencies that are available for conversion
+exchange_rate_list = [0.7, 0.6, 76.95, 0.5, 0.95, 0.88, 0.64, 4.52, 6.07, 1, 14, 0.95, 5.42, 6.25, 801.03, 6.04, 51.91, 51.37, 3.65, 10.3]
+last_updated = "11/08/2021 14:10"
+still_converting = True
+
+while still_converting:     # while user wants to keep converting, run the loop
+    print("These are the currencies available for converting: \n"
+          "US Dollar (USD), Euro (EUR), Yen (JPY), Pound Sterling (GBP), Australian Dollar (AUD), Canadian (CAD), Swiss Franc (CHF), Chinese Renminbi (CNY), Swedish Krona (SEK), New Zealand Dollar (NZD), Mexican Peso (MEX), Singapore Dollar (SGD), Hong Kong Dollar (HKD), Norwegian Krone (NOK), South Korea Won (KRW), Turkish Lira (TRY), Indian Rupee (INR), Russian Ruble (RUB), Brazilian Real (BRL), South African Rand (ZAR)")
+
+    while True:     # this loop forces the user to enter a valid input
+        selected_currency_from = input("Please enter a currency code (e.g. US = USD) that you would like to convert from:").strip().upper()
+        if selected_currency_from in currency_list:     # checks if the selected currency is available for conversion
+            break
+        else:       # if not, ask the user to enter a currency code that is available for conversion
+            print("Please enter a currency code that is available for conversion.")
+
+    while True:      # this loop forces the user to enter a valid input
+        selected_currency_to = input("Please enter a currency code (e.g. US = USD) that you would like to convert to:").strip().upper()
+        if selected_currency_to in currency_list:       # checks if the selected currency is available for conversion
+            break
+        else:       # if not, ask the user to enter a currency code that is available for conversion
+            print("Please enter a currency code that is available for conversion.")
+
+    while True:     # this loop forces the user to enter a valid input
+        try:        # when user enters a number, it will try to convert it using float
+            converting_value = float(input("Please enter an amount that you would like to convert:"))
+            if converting_value < 0:        # checks if the amount is in the right range
+                print("Please enter a value larger than 0")     # if not, ask the user tp enter an amount in the right range
+            else:
+                break
+        except ValueError:      # when the input is not a string, it prints out an error message and rerun the loop
+            print("Please enter a valid input")
